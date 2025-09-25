7a6050a84b6ac6ecd7d4d33c2568ec22325ab031
Jose Luis Burbano <jose.burbanob@campusucc.edu.co>
2025-09-25 11:21:23 -0500

    App Solicitudes Completa

--- Taller_Formularios/solicitudes/views.py
+++ Taller_Formularios/solicitudes/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SolicitudForm
 
-# Create your views here.
+def solicitud_view(request):
+    if request.method == 'POST':
+        form = SolicitudForm(request.POST, request.FILES) 
+        if form.is_valid():
+            form.save()
+            return redirect('solicitud_confirmacion')
+    else:
+        form = SolicitudForm()
+    
+    return render(request, 'solicitudes/solicitud_form.html', {'form': form})
+
+def solicitud_confirmacion(request):
+    return render(request, 'solicitudes/solicitud_confirmacion.html')
